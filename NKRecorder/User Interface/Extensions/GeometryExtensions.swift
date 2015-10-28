669830b0b5d5e14b977923be2176dff74a903fb0
Nurishka <nurim98@gmail.com>
2015-10-28 00:06:20 -0700

    made GeometryExtensions internal to NKRecorder

--- NKRecorder/User Interface/Extensions/GeometryExtensions.swift
+++ NKRecorder/User Interface/Extensions/GeometryExtensions.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 
-public let π = CGFloat(M_PI)
+let π = CGFloat(M_PI)
 
 extension CGRect {
-    public var center: CGPoint {
+    var center: CGPoint {
         return CGPoint(x: midX, y: midY)
     }
     
-    public init(center: CGPoint, width: CGFloat, height: CGFloat) {
+    init(center: CGPoint, width: CGFloat, height: CGFloat) {
         let origin = CGPoint(x: center.x - width / 2, y: center.y - height / 2)
         self.init(origin: origin, size: CGSize(width: width, height: height))
     }
     
-    public init(center: CGPoint, radius: CGFloat) {
+    init(center: CGPoint, radius: CGFloat) {
         self.init(center: center, width: radius * 2, height: radius * 2)
     }
 }
 
 extension UIBezierPath {
-    public convenience init(circleCenter: CGPoint, radius: CGFloat) {
+    convenience init(circleCenter: CGPoint, radius: CGFloat) {
         self.init(ovalInRect: CGRect(center: circleCenter, radius: radius))
     }
 }
